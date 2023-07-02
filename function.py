c50b7dcb286a4eae2de31937ef29e9717608f442
Les1a <102607924+Les1a@users.noreply.github.com>
2023-07-02 18:12:15 +0800

    Update function.py

--- function.py
+++ function.py
@@ -4,11 +4,10 @@ def get_unique_file_name(file_name):
     if not os.path.isdir(file_name):
         return file_name
     else:
-        # base_name, ext = os.path.splitext(file_name)
+        base_name, ext = os.path.splitext(file_name)
         counter = 1
         while True:
-            # numbered_file_name = f"{base_name}_{counter}{ext}"
-            numbered_file_name = f"{file_name}_{counter}"
+            numbered_file_name = f"{base_name}_{counter}{ext}"
             if not os.path.isfile(numbered_file_name):
                 return numbered_file_name
             counter += 1
