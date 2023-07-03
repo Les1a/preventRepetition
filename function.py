28167a93c50698ff4edce625d672c1bc98808c0c
Hiro <102607924+Les1a@users.noreply.github.com>
2023-07-03 13:59:28 +0800

    Update function.py

--- function.py
+++ function.py
@@ -11,3 +11,17 @@ def get_unique_file_name(file_name):
             if not os.path.isfile(numbered_file_name):
                 return numbered_file_name
             counter += 1
+
+def get_unique_dir_name(dir_name):
+    import os
+    root = './output'
+    '''避免重名'''
+    if not os.path.isdir(os.path.join(root, dir_name)):
+        return dir_name
+    else:
+        counter = 1
+        while True:
+            numbered_dir_name = f"{dir_name}_{counter}"
+            if not os.path.isdir(os.path.join(root, numbered_dir_name)):
+                return numbered_dir_name
+            counter += 1
